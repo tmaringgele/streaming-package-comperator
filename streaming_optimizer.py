ac192622c0100cb8825440670fc6ebf483d83cd9
tmaringgele <tmaringgele@outlook.at>
2024-12-22 11:56:19 +0100

    put in py file

--- streaming_optimizer.py
+++ streaming_optimizer.py
@@ -1,5 +1,67 @@
 import pulp
+import pandas as pd
 from datetime import datetime, timedelta
+
+
+def preprocess_data(game_ids_of_interest, streaming_offers_raw, streaming_packages_raw, games_raw):
+
+    ## Preprocess data (make everthing as small as possible)
+
+    ### Filter packages to include only relevant ones
+    # Identify relevant package IDs from offers
+    relevant_package_ids = streaming_offers_raw[streaming_offers_raw['game_id'].isin(game_ids_of_interest)]['streaming_package_id'].unique()
+
+    # Filter packages to include only relevant ones
+    filtered_packages = streaming_packages_raw[streaming_packages_raw['id'].isin(relevant_package_ids)]
+
+    filtered_packages['yearly_price'] = filtered_packages['monthly_price_yearly_subscription_in_cents'] * 12
+    filtered_packages = filtered_packages.drop(columns=['monthly_price_yearly_subscription_in_cents'])
+
+
+    games = games_raw[games_raw['id'].isin(game_ids_of_interest)]
+
+    # Filter offers to include only the games of interest
+    filtered_offers = streaming_offers_raw[streaming_offers_raw['game_id'].isin(game_ids_of_interest)]
+
+
+    # Extract unique package IDs
+    packages = filtered_packages['id'].unique().tolist()
+
+    # Create game_dates dictionary
+    games.loc[:, 'starts_at'] = pd.to_datetime(games['starts_at'])
+    game_dates = games.set_index('id')['starts_at'].apply(lambda x: x.date()).to_dict()
+
+
+    # Create C_month and C_year dictionaries, dropping packages with NA for the respective type
+    C_month = filtered_packages.dropna(subset=['monthly_price_cents']) \
+        .set_index('id')['monthly_price_cents'].to_dict()
+    C_year = filtered_packages.dropna(subset=['yearly_price']) \
+        .set_index('id')['yearly_price'].to_dict()
+
+
+    # Create P_g dictionary
+    P_g = filtered_offers.groupby('game_id')['streaming_package_id'].apply(list).to_dict()
+
+
+    ## Remove all games with no offer
+    games_with_no_offers = []
+    for game_id in game_ids_of_interest:
+        if game_id not in P_g:
+            games_with_no_offers.append(game_id)
+
+    game_ids_of_interest = [game_id for game_id in game_ids_of_interest if game_id in P_g]
+
+    result = {
+        "packages": packages,
+        "games": game_ids_of_interest,
+        "game_dates": game_dates,
+        "C_month": C_month,
+        "C_year": C_year,
+        "P_g": P_g,
+        'games_with_no_offers': games_with_no_offers
+    }
+
+    return result
 
 
 def optimize_streaming_packages(packages, games, game_dates, C_month, C_year, P_g):
