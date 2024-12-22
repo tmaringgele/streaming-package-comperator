323e5aa4042073169f4cf35cac87003701062d20
tmaringgele <tmaringgele@outlook.at>
2024-12-22 14:57:43 +0100

    Revert "setting up vercel"
    This reverts commit 7e89ea84266b66135fc661dc7a1f5915aec0162d.

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -2,8 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [sveltekit()],
   build: {
     outDir: 'build'
   }
+=======
+	plugins: [sveltekit()]
+>>>>>>> parent of 7e89ea8 (setting up vercel)
 });
